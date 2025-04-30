993a5a06ee4809df57e2360bc01452161d790e93
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-04-30 18:12:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,9 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
+     <p>Enter your name</p>
       <input
         type="text"
-        placeholder="Enter your name"
         value={name}
         onChange={handleChange}
       />
